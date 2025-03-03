6b5028409bd6fd57eb043cf15b6ca3f6ca9e86b9
Miyamura <felipesilsou1101@gmail.com>
2025-03-03 18:18:35 -0300

    Criação da Tabela

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using TodoApi.Context;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConexaoPadrao"))); // UserNpgsql -> Para o Postgres
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
